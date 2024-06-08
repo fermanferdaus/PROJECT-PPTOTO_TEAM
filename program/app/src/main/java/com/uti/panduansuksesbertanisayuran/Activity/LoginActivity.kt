d8c8ba68b26d5ad03965c5a24108e74c6301ac47
pebrio00 <akhmadfebrio62@gmail.com>
2024-06-09 01:01:06 +0700

    febri: menambahkan deklarasi binding databaseHelper pada login activity

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/LoginActivity.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/LoginActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.uti.panduansuksesbertanisayuran.Class.db
 import com.uti.panduansuksesbertanisayuran.R
 
 class LoginActivity : AppCompatActivity() {
+    // Deklarasi binding untuk data Helper
+    private lateinit var databaseHelper: db
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
