0539d645780a64e40397c257330b5750ae154f24
pebrio00 <akhmadfebrio62@gmail.com>
2024-06-09 02:08:05 +0700

    febri: menambahkan Binding pada SignUpActivity

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/SignupActivity.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/SignupActivity.kt
@@ -6,10 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.uti.panduansuksesbertanisayuran.R
+import com.uti.panduansuksesbertanisayuran.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//  Binding SignupActivity
+        val binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
