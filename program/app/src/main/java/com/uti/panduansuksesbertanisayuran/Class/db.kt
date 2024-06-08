c04e37d1d5b7ece699b888804074616658f1ac8a
pebrio00 <akhmadfebrio62@gmail.com>
2024-06-08 20:49:08 +0700

    febri: membuat companion object

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Class/db.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Class/db.kt
@@ -1,4 +1,12 @@
 package com.uti.panduansuksesbertanisayuran.Class
 
 class db {
+    companion object {
+        private const val DATABASE_NAME = "UserDatabase.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "data"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_USERNAME = "username"
+        private const val COLUMN_PASSWORD = "password"
+    }
 }
