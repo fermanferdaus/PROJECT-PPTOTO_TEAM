444a30daa7e0b874ae27ea486c351a35064c2100
unknown <fermanf91@gmail.com>
2024-06-08 18:30:31 +0700

    master: melakukan inisialisasi contentList dan onItemClick

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/ContentAdapter.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/ContentAdapter.kt
@@ -1,4 +1,26 @@
 package com.uti.panduansuksesbertanisayuran.Activity
 
-class ContentAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.uti.panduansuksesbertanisayuran.R
+
+class ContentAdapter(
+    private val contentList: List<Pair<String, String>>, // Pair of title and URL
+    private val onItemClick: (String, String) -> Unit // Callback with title and URL
+) : RecyclerView.Adapter<ContentAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContentAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
