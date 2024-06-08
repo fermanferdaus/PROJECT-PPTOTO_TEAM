98277582d9410d51f7825749ba0e740349c6f3ba
unknown <fermanf91@gmail.com>
2024-06-08 18:35:04 +0700

    master: membuat class ViewHolder untuk memegang referensi tampilan setiap item dalam daftar

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/ContentAdapter.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Activity/ContentAdapter.kt
@@ -11,6 +11,12 @@ class ContentAdapter(
     private val contentList: List<Pair<String, String>>, // Pair of title and URL
     private val onItemClick: (String, String) -> Unit // Callback with title and URL
 ) : RecyclerView.Adapter<ContentAdapter.ViewHolder>(){
+
+//  ViewHolder untuk memegang referensi tampilan setiap item dalam daftar
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val itemText: TextView = view.findViewById(R.id.item_text)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentAdapter.ViewHolder {
         TODO("Not yet implemented")
     }
