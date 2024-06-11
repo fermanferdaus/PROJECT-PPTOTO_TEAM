713105c3ad6c40910346578fc6768831c37ac8f8
fermanferdaus <fermanf91@gmail.com>
2024-06-11 22:17:56 +0700

    master: membuat ViewHolder baru ketika RecyclerView membutuhkannya

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Class/ContentAdapter.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Class/ContentAdapter.kt
@@ -1,6 +1,8 @@
 package com.uti.panduansuksesbertanisayuran.Class
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.uti.panduansuksesbertanisayuran.R
@@ -8,5 +10,10 @@ import com.uti.panduansuksesbertanisayuran.R
 class ContentAdapter {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val itemText: TextView = view.findViewById(R.id.item_text) // TextView untuk menampilkan judul item
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_content, parent, false) // Meng-inflate layout item_list_content
+        return ViewHolder(view)
     }
 }
