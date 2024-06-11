400adcf947040033fbd11ecff76fafe60cdad6cf
fermanferdaus <fermanf91@gmail.com>
2024-06-11 22:15:51 +0700

    master: membuat Kelas ViewHolder untuk mengelola tampilan item dalam RecyclerView

--- program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Class/ContentAdapter.kt
+++ program/app/src/main/java/com/uti/panduansuksesbertanisayuran/Class/ContentAdapter.kt
@@ -1,4 +1,12 @@
 package com.uti.panduansuksesbertanisayuran.Class
 
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.uti.panduansuksesbertanisayuran.R
+
 class ContentAdapter {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val itemText: TextView = view.findViewById(R.id.item_text) // TextView untuk menampilkan judul item
+    }
 }
